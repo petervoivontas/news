25b064bc9be5bf3968ef82def02bde43f291fd50
Peter Voivontas <petervoivontas@outlook.com>
2018-08-08 19:20:32 +0300

    Make directory for icons

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import menuicon from './menuicon.png';
+import logo from './icons/favicon.ico'
+import menuicon from './icons/menuicon.png';
 import './Header.css';
 
 export class Header extends React.Component {
     render ()  {
         return (
             <header className='header'>
-                <img src='favicon.ico' alt='logo' className='logo'/>
+                <img src={logo} alt='logo' className='logo'/>
                 <img src={menuicon} alt='menu icon' className='menuicon'/>
             </header>
         )
