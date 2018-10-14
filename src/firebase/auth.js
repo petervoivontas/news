cd37e6b39989568f115b05832fb8a8c4f6e2c40c
Peter Voivontas <petervoivontas@outlook.com>
2018-10-14 22:58:45 +0300

    asdfsdf

---
+++ src/firebase/auth.js
@@ -0,0 +1,7 @@
+import firebase from 'firebase';
+import auth from './firebase';
+import 'firebase/app';
+
+export const signUp = (email, password) => {
+    auth.do
+}
--- src/views/Signup.js
+++ src/views/Signup.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
+import firebase, {auth, provider} from '../firebase/firebase';
 
 // Components
 import {Header} from './Header';
@@ -28,65 +29,78 @@ export class Signup extends React.Component {
         signupPage.fadeIn(500);
     }
 
-    handleSignup () {
-        const signupPage = $('.signupPage');
-        const name = $('#username').val().trim();
-        const email = $('#email').val().trim();
-        const password = $('#password').val().trim();
+    // handleSignup () {
+    //     const signupPage = $('.signupPage');
+    //     const name = $('#username').val().trim();
+    //     const email = $('#email').val().trim();
+    //     const password = $('#password').val().trim();
 
-        if (name !== '' && email !== '' && password !== '') {
-            fetch(`http://${this.state.ip}:4000/users/signup`, {
-                method: 'POST',
-                body: JSON.stringify({
-                    name: document.getElementById('username').value,
-                    email: document.getElementById('email').value,
-                    password: document.getElementById('password').value
-                }),
-                headers:{
-                    'Content-Type': 'application/json'
-                },
-            }).then(response => {
-                if (response.ok) {
-                    return response.json();
-                }
-                throw new Error('Request failed');
-            }, networkError => console.log(networkError)
-            ).then(jsonResponse => {
-                console.log(jsonResponse);
-                signupPage.fadeOut(500);
-                setTimeout(()=> {
-                    ReactDOM.render(<Home firstRender={false} name={jsonResponse.name} email={jsonResponse.email} password={jsonResponse.password}/>, document.getElementById('app'));
-                }, 500);
-            });
-        } else if (name === '' && email === '' && password === '') {
-            console.log('Enter your info');
-        } else if (name === '') {
-            console.log('Name cannot be empty');
-        } else if (email === '') {
-            console.log('Email cannot be empty');
-        } else {
-            console.log('Password cannot be empty');
-        }
+    //     if (name !== '' && email !== '' && password !== '') {
+    //         fetch(`http://${this.state.ip}:4000/users/signup`, {
+    //             method: 'POST',
+    //             body: JSON.stringify({
+    //                 name: document.getElementById('username').value,
+    //                 email: document.getElementById('email').value,
+    //                 password: document.getElementById('password').value
+    //             }),
+    //             headers:{
+    //                 'Content-Type': 'application/json'
+    //             },
+    //         }).then(response => {
+    //             if (response.ok) {
+    //                 return response.json();
+    //             }
+    //             throw new Error('Request failed');
+    //         }, networkError => console.log(networkError)
+    //         ).then(jsonResponse => {
+    //             console.log(jsonResponse);
+    //             signupPage.fadeOut(500);
+    //             setTimeout(()=> {
+    //                 ReactDOM.render(<Home firstRender={false} name={jsonResponse.name} email={jsonResponse.email} password={jsonResponse.password}/>, document.getElementById('app'));
+    //             }, 500);
+    //         });
+    //     } else if (name === '' && email === '' && password === '') {
+    //         console.log('Enter your info');
+    //     } else if (name === '') {
+    //         console.log('Name cannot be empty');
+    //     } else if (email === '') {
+    //         console.log('Email cannot be empty');
+    //     } else {
+    //         console.log('Password cannot be empty');
+    //     }
+    // }
+
+    handleSignup () {
+        auth.signInWithPopup(provider)
+            .then((result) => {
+                const user = result.user;
+                this.setState({
+                    user: user
+                });
+            })
     }
 
     render () {
+        // return (
+        //     <div className='signupPage'>
+        //         <Header page='signup' />
+        //         <div>
+        //             <p>Your opinion matters</p>
+        //             <hr />
+        //             <p>Sign up to Nuntium</p>
+        //         </div>
+        //         <div className='form'>
+        //             <input id='username' title='username' type='text' autoComplete='on' placeholder='Your name'></input>
+        //             <input id='email' title='email' type='email' autoComplete='on' placeholder='email'></input>
+        //             <input id='password' title='password' type='password' autoComplete='on' placeholder='password'></input>
+        //             <br />
+        //             <br />
+        //             <button id='submit' onClick={this.handleSignup}>Sign Up</button>
+        //         </div>
+        //     </div>
+        // )
         return (
-            <div className='signupPage'>
-                <Header page='signup' />
-                <div>
-                    <p>Your opinion matters</p>
-                    <hr />
-                    <p>Sign up to Nuntium</p>
-                </div>
-                <div className='form'>
-                    <input id='username' title='username' type='text' autoComplete='on' placeholder='Your name'></input>
-                    <input id='email' title='email' type='email' autoComplete='on' placeholder='email'></input>
-                    <input id='password' title='password' type='password' autoComplete='on' placeholder='password'></input>
-                    <br />
-                    <br />
-                    <button id='submit' onClick={this.handleSignup}>Sign Up</button>
-                </div>
-            </div>
+            <button onClick={this.handleSignup}>Sign Up</button>
         )
     }
 }
